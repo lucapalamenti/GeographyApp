72ec0ef852476f7caeeca8ceb4e074cff05b47db
lucapalamenti <luca.palamenti@gmail.com>
2025-01-12 23:54:52 -0500

    All 'Click' gamemodes implemented

--- app/static/js/gamemodes.js
+++ app/static/js/gamemodes.js
@@ -12,8 +12,16 @@ export const gamemodeMap = {
 const svg = document.querySelector('SVG');
 const promptBar1 = document.getElementById('prompt-bar-1');
 const promptBar2 = document.getElementById('prompt-bar-2');
+let tally;
+let numCorrect = 0;
+let guesses = 1;
 
-function click( shapeNames ) {
+function click ( shapeNames ) { clickGamemodes( shapeNames, false, false ); }
+function clickDisappear ( shapeNames ) { clickGamemodes( shapeNames, true, false ); }
+function clickEndless ( shapeNames ) { clickGamemodes( shapeNames, true, true ); }
+
+function clickGamemodes( shapeNames, disappear, endless ) {
+    const totalShapes = endless ? Infinity : shapeNames.length;
     const strong = promptBar1.querySelector('STRONG');
     let arr = shapeNames;
     // Shuffle the array
@@ -26,26 +34,65 @@ function click( shapeNames ) {
     
     let current = arr.pop();
     strong.textContent = current;
+    let endlessQueue = [current];
     promptBar1.style.display = "flex";
+    tally = document.getElementById('tally');
+    tally.textContent = `${numCorrect}/${totalShapes}`;
 
     svg.addEventListener('click', e => {
         const target = e.target;
         if ( target.nodeName === "polygon" && target.classList.length === 1 ) {
             // Correct region clicked
             if ( target.className.baseVal === current.split(' ').join('_') ) {
+                if ( guesses === 1 ) {
+                    numCorrect++;
+                } else {
+                    guesses = 1;
+                }
                 document.querySelectorAll(`.${target.className.baseVal}`).forEach( polygon => {
-                    polygon.classList.add('polygonCorrect');
-                    polygon.style.fill = 'green';
+                    polygon.style.fill = 'rgb(0, 255, 0)';
+                    // Disappear gamemode
+                    if ( disappear ) {
+                        polygon.classList.add('polygonClicked');
+                        setTimeout( function() {
+                            polygon.style.transition = 'fill 1s ease';
+                            polygon.style.fill = '';
+                            setTimeout( function() {
+                                polygon.style.transition = '';
+                                polygon.classList.remove('polygonClicked');
+                            }, 1000 );
+                        }, 1000 );
+                    }
+                    // Standard gamemode
+                    else {
+                        polygon.classList.add('polygonCorrect');
+                    }
+                    
                 });
-                if ( !(current = arr.pop()) ) {
-                    console.log( "YOU WIN!" );
+                tally.textContent = `${numCorrect}/${totalShapes}`;
+                if ( !( current = arr.pop() ) ) {
+                    win();
                     strong.textContent = '-';
                 } else {
                     strong.textContent = current;
+                    // Endless gamemode
+                    if ( endless ) {
+                        endlessQueue.push( current );
+                        if ( endlessQueue.length > arr.length) {
+                            // Pick a random index in the front half of the waiting queue
+                            const index1 = Math.floor( Math.random() * endlessQueue.length / 2 );
+                            // Pick a random index in the back half of the current queue
+                            const index2 = Math.floor( Math.random() * arr.length / 2 );
+                            // Insert the value from waiting queue into the current queue
+                            arr = arr.slice( 0, index2 ).concat( endlessQueue.at( index1 ), arr.slice( index2 ) );
+                            endlessQueue = endlessQueue.slice(0, index1).concat( endlessQueue.slice( index1 + 1 ) );
+                        }
+                    }
                 }
             }
             // Incorrect region clicked
             else {
+                guesses++;
                 document.querySelectorAll(`.${target.className.baseVal}`).forEach( polygon => {
                     polygon.classList.add('polygonClicked');
                     polygon.style.fill = 'red';
@@ -63,24 +110,12 @@ function click( shapeNames ) {
     });
 }
 
-function clickDisappear( shapeNames ) {
-    clickGamemodes( shapeNames );
-}
+function type( shapeNames ) { typeGamemodes( shapeNames, false, false ); }
+function typeHard( shapeNames ) { typeGamemodes( shapeNames, true, false ); }
+function typeEndless( shapeNames ) { typeGamemodes( shapeNames, false, true ); }
 
-function clickEndless( shapeNames ) {
-    clickGamemodes( shapeNames );
-}
-
-function type( shapeNames ) {
-    typeGamemodes( shapeNames );
-}
-
-function typeHard( shapeNames ) {
-    typeGamemodes( shapeNames );
-}
-
-function typeEndless( shapeNames ) {
-    typeGamemodes( shapeNames );
+function typeGamemodes( shapeNames, hard, endless ) {
+    promptBar2.style.display = "flex";
 }
 
 function noMap( shapeNames ) {
@@ -91,13 +126,6 @@ function noList( shapeNames ) {
 
 }
 
-/**
- * @param {Array} shapeNames 
- */
-function clickGamemodes( shapeNames ) {
-    
-}
-
-function typeGamemodes( shapeNames ) {
-    promptBar2.style.display = "flex";
+function win() {
+    console.log( "YOU WIN!" );
 }
