1782c72af55bf4bf56ad20cff198a79b8776f25d
lucapalamenti <luca.palamenti@gmail.com>
2025-01-29 15:10:58 -0500

    Update Map object

--- app/src/api/db/models/Map.js
+++ app/src/api/db/models/Map.js
@@ -2,10 +2,12 @@ module.exports = class Map {
     map_id = null;
     map_name = null;
     map_thumbnail = null;
+    map_primary_color = null;
 
     constructor ( data ) {
         this.map_id = data.map_id;
         this.map_name = data.map_name;
         this.map_thumbnail = data.map_thumbnail;
+        this.map_primary_color = data.map_primary_color;
     }
 };
