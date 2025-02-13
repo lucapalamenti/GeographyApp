1fc731186526c99f4064b689db63741f516313d6
lucapalamenti <luca.palamenti@gmail.com>
2025-02-12 23:05:10 -0500

    Test parse file

---
+++ app/static/js/parse/Test_Map.js
@@ -0,0 +1,20 @@
+import APIClient from '../APIClient.js';
+import format from '../format.js';
+
+const shapeNames = ["topLeft","topRight","bottomLeft","bottomRight"];
+const shapePoints = [
+    "100,100 300,100 300,300 100,300 100,100",
+    "300,100 500,100 500,300 300,300",
+    "100,300 300,300 300,500 100,500",
+    "300,300 500,300 500,500 300,500"
+];
+const b1 = document.querySelector('#b1');
+
+b1.addEventListener('click', () => {
+    for ( let i = 0; i < shapeNames.length; i++ ) {
+        const data = {
+            string: `${i+1}, 0, '${shapeNames[i]}', ST_GEOMFROMTEXT('${format.pointsToMultiPolygon( [shapePoints[i]] )}')`
+        };
+        APIClient.printShapesToFile( data );
+    }
+});
