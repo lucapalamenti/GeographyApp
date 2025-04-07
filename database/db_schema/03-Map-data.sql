21e58a242c85d7e37680f13808b87da4af36b3dc
lucapalamenti <luca.palamenti@gmail.com>
2025-04-07 09:57:15 -0400

    Updated data to 2023

--- database/db_schema/03-Map-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `map` (`map_id`, `map_scale`, `map_name`, `map_thumbnail`, `map_primary_color`) VALUES (1, 1, 'US States Polygon', 'US_States_Polygon_Thumbnail.png', '0,0,0');
-INSERT INTO `map` (`map_id`, `map_scale`, `map_name`, `map_thumbnail`, `map_primary_color`) VALUES (2, 27.34355, 'US States', 'US_States_Thumbnail.png', '10,49,97');
-INSERT INTO `map` (`map_id`, `map_scale`, `map_name`, `map_thumbnail`, `map_primary_color`) VALUES (3, 27.34355, 'US Counties', 'US_Counties_Thumbnail.png', '10,49,97');
