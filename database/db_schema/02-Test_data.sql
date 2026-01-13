c2d1c9b2edc99df63c8bd89557a04a268cf29d1d
lucapalamenti <luca.palamenti@gmail.com>
2026-01-12 21:15:52 -0500

    fix comma

--- database/db_schema/02-Test_data.sql
+++ database/db_schema/02-Test_data.sql
@@ -5,14 +5,14 @@ INSERT INTO `region` (`region_id`, `region_name`, `region_parent_id`) VALUES (2,
 INSERT INTO `region` (`region_id`, `region_name`, `region_parent_id`) VALUES (3, 'Bottom Left', null);
 INSERT INTO `region` (`region_id`, `region_name`, `region_parent_id`) VALUES (4, 'Bottom Right', null);
 
-INSERT INTO `polygon` (`polygon_id`, `polygon_region_id` `polygon_points`) VALUES (1, 1, ST_GEOMFROMTEXT('POLYGON((0 0,10 0,10 10,0 10,0 0))'));
-INSERT INTO `polygon` (`polygon_id`, `polygon_region_id` `polygon_points`) VALUES (2, 1, ST_GEOMFROMTEXT('POLYGON((20 20,25 20,25 25,20 25,20 20))'));
-INSERT INTO `polygon` (`polygon_id`, `polygon_region_id` `polygon_points`) VALUES (3, 2, ST_GEOMFROMTEXT('POLYGON((0 0,10 0,10 10,0 10,0 0))'));
-INSERT INTO `polygon` (`polygon_id`, `polygon_region_id` `polygon_points`) VALUES (4, 2, ST_GEOMFROMTEXT('POLYGON((20 20,25 20,25 25,20 25,20 20))'));
-INSERT INTO `polygon` (`polygon_id`, `polygon_region_id` `polygon_points`) VALUES (5, 3, ST_GEOMFROMTEXT('POLYGON((0 0,10 0,10 10,0 10,0 0))'));
-INSERT INTO `polygon` (`polygon_id`, `polygon_region_id` `polygon_points`) VALUES (6, 3, ST_GEOMFROMTEXT('POLYGON((20 20,25 20,25 25,20 25,20 20))'));
-INSERT INTO `polygon` (`polygon_id`, `polygon_region_id` `polygon_points`) VALUES (7, 4, ST_GEOMFROMTEXT('POLYGON((0 0,10 0,10 10,0 10,0 0))'));
-INSERT INTO `polygon` (`polygon_id`, `polygon_region_id` `polygon_points`) VALUES (8, 4, ST_GEOMFROMTEXT('POLYGON((20 20,25 20,25 25,20 25,20 20))'));
+INSERT INTO `polygon` (`polygon_id`, `polygon_region_id`, `polygon_points`) VALUES (1, 1, ST_GEOMFROMTEXT('POLYGON((0 0,10 0,10 10,0 10,0 0))'));
+INSERT INTO `polygon` (`polygon_id`, `polygon_region_id`, `polygon_points`) VALUES (2, 1, ST_GEOMFROMTEXT('POLYGON((20 20,25 20,25 25,20 25,20 20))'));
+INSERT INTO `polygon` (`polygon_id`, `polygon_region_id`, `polygon_points`) VALUES (3, 2, ST_GEOMFROMTEXT('POLYGON((0 0,10 0,10 10,0 10,0 0))'));
+INSERT INTO `polygon` (`polygon_id`, `polygon_region_id`, `polygon_points`) VALUES (4, 2, ST_GEOMFROMTEXT('POLYGON((20 20,25 20,25 25,20 25,20 20))'));
+INSERT INTO `polygon` (`polygon_id`, `polygon_region_id`, `polygon_points`) VALUES (5, 3, ST_GEOMFROMTEXT('POLYGON((0 0,10 0,10 10,0 10,0 0))'));
+INSERT INTO `polygon` (`polygon_id`, `polygon_region_id`, `polygon_points`) VALUES (6, 3, ST_GEOMFROMTEXT('POLYGON((20 20,25 20,25 25,20 25,20 20))'));
+INSERT INTO `polygon` (`polygon_id`, `polygon_region_id`, `polygon_points`) VALUES (7, 4, ST_GEOMFROMTEXT('POLYGON((0 0,10 0,10 10,0 10,0 0))'));
+INSERT INTO `polygon` (`polygon_id`, `polygon_region_id`, `polygon_points`) VALUES (8, 4, ST_GEOMFROMTEXT('POLYGON((20 20,25 20,25 25,20 25,20 20))'));
 
 INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_Region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_scaleX`, `mapRegion_scaleY`) VALUES (0, 1, 'Left', 0, 0, 1.25, 0.75);
 INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_Region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_scaleX`, `mapRegion_scaleY`) VALUES (0, 2, 'Right', 10, 0, 1.25, 0.75);
