c87fa6fac86a91f67585374fa8785ac0b77f7168
lucapalamenti <luca.palamenti@gmail.com>
2026-01-20 18:39:54 -0500

    Update map INSERT statements
    Add surrounding "outside" regions to state counties maps.

--- database/db_schema/04-Map-Delaware_Counties.sql
+++ database/db_schema/04-Map-Delaware_Counties.sql
@@ -1,4 +1,45 @@
 INSERT INTO `map` (`map_id`, `map_scale`, `map_name`, `map_thumbnail`, `map_primary_color_R`, `map_primary_color_G`, `map_primary_color_B`, `map_is_custom`) VALUES (11, 507.141201, 'Delaware Counties', 'Delaware_Counties_Thumbnail.png', 231,200,52, 0);
-INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_scaleX`, `mapRegion_scaleY`) VALUES (11, 422, "Delaware", 0.000000, 0.000000, 1.000000, 1.000000);
-INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_scaleX`, `mapRegion_scaleY`) VALUES (11, 421, "Delaware", 0.028589, 0.591912, 1.000000, 1.000000);
-INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_scaleX`, `mapRegion_scaleY`) VALUES (11, 423, "Delaware", 0.066094, 1.096991, 1.000000, 1.000000);
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`) VALUES (11, 422, "Delaware", 0.000000, 0.000000);
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`) VALUES (11, 421, "Delaware", 0.028589, 0.591912);
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`) VALUES (11, 423, "Delaware", 0.066094, 1.096991);
+
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 3020, "Virginia", -1.354782, 1.243134, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 2931, "Virginia", -1.383381, 1.131018, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 2953, "Virginia", -1.747093, 0.976737, "outside");
+
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 2349, "Pennsylvania", -1.682186, -0.288451, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 2363, "Pennsylvania", -0.313578, -0.504208, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 2371, "Pennsylvania", 0.190984, -0.282947, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 2384, "Pennsylvania", -0.932726, -0.595982, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 2415, "Pennsylvania", -1.348528, -0.48393, "outside");
+
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1299, "Maryland", -1.051468, 0.752233, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1300, "Maryland", -1.106664, 0.147095, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1321, "Maryland", -0.922596, 0.583724, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1301, "Maryland", -0.913065, 1.337387, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1302, "Maryland", -0.226149, 0.869881, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1303, "Maryland", -1.522266, 0.147624, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1304, "Maryland", -0.444383, 0.146116, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1305, "Maryland", -1.485323, 1.416321, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1306, "Maryland", -0.547464, 1.418994, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1309, "Maryland", -0.780555, 0.146923, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1310, "Maryland", -1.398216, 0.587328, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1311, "Maryland", -0.489629, 0.570794, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1312, "Maryland", -1.738388, 0.607104, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1313, "Maryland", -1.297434, 0.885272, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1314, "Maryland", -0.587303, 0.721705, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1315, "Maryland", -1.085459, 1.661056, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1317, "Maryland", -0.603562, 1.118658, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1319, "Maryland", -0.128403, 1.598021, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1320, "Maryland", 0.127068, 1.73402, "outside");
+
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1879, "New Jersey", 0.803626, 0.136762, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1881, "New Jersey", 0.728903, -0.430579, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1882, "New Jersey", 0.648889, -0.195846, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1883, "New Jersey", 0.816903, 0.644212, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1884, "New Jersey", 0.370592, 0.338048, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1886, "New Jersey", 0.373833, -0.061601, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1893, "New Jersey", 1.23579, -0.416522, "outside");
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 1895, "New Jersey", 0.230609, 0.056267, "outside");
+
+INSERT INTO `mapRegion` (`mapRegion_map_id`, `mapRegion_region_id`, `mapRegion_parent`, `mapRegion_offsetX`, `mapRegion_offsetY`, `mapRegion_type`) VALUES (11, 424, "United States", -1.330862, 1.054546, "outside");
