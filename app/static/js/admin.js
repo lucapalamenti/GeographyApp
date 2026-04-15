6159da9715de1f6f2bc95f571d412cba778c1eea
lucapalamenti <luca.palamenti@gmail.com>
2026-04-14 22:52:05 -0400

    properties of features are displayed on screen

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -8,6 +8,24 @@ window.onload = () => {
 }
 
 const uploadThumbnail = document.getElementById("upload-file-form");
+const fileStream = document.getElementById("file-stream");
+
+uploadThumbnail.addEventListener("change", e => {
+    const file = e.target.files[0];
+    if ( file ) {
+        const reader = new FileReader();
+        reader.onload = function( event ) {
+            try {
+                const json = JSON.parse( event.target.result );
+                fileStream.innerHTML = JSON.stringify( json["features"][0]["properties"], null, 2 );
+            } catch ( err ) {
+                console.error( "Invalid JSON file!", err );
+            }
+        };
+        reader.readAsText( file );
+    }
+});
+
 uploadThumbnail.addEventListener("submit", async e => {
     e.preventDefault();
     await APIClient.uploadFile( e.target ).then( async res => {
