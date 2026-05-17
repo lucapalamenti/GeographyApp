7bd3ea547fe54acd02691605818a2e3b87515fbb
lucapalamenti <luca.palamenti@gmail.com>
2026-05-17 11:52:16 -0400

    FeatureCollection can be much simpler for current implementation

--- app/src/api/models/FeatureCollection.js
+++ app/src/api/models/FeatureCollection.js
@@ -5,29 +5,16 @@ class FeatureCollection {
     type = "FeatureCollection";
     /** @type {String} */
     name = null;
-    /** @type {Object} */
-    crs = {};
     /** @type {Array<Feature>} */
     features = [];
 
     /**
      * 
-     * @param {string} name 
+     * @param {FeatureCollection} data 
      */
-    constructor ( name ) {
-        this.name = name;
-    }
-
-    /**
-     * Creates and adds a new Feature to the FeatureCollection, and returns it
-     * @param {Object} properties 
-     * @param {SQLGeometry} geometry 
-     * @returns {Feature}
-     */
-    createFeature( properties = undefined, geometry = undefined ) {
-        const feature = new Feature( properties, geometry );
-        this.features.push( feature );
-        return feature;
+    constructor ( data ) {
+        this.name = data.name;
+        this.features = data.features.map( f => new Feature( f ) );
     }
 }
 
@@ -41,40 +28,10 @@ class Feature {
 
     /**
      * 
-     * @param {Object} properties 
-     * @param {SQLGeometry} geometry 
+     * @param {Feature} data
      */
-    constructor ( properties = undefined, geometry = undefined ) {
-        if ( properties ) this.setProperties( properties );
-        if ( geometry ) this.setGeometry( geometry );
-    }
-
-    /**
-     * Sets the properties field to the given properties object
-     * @param {Object} properties 
-     */
-    setProperties( properties ) {
-        this.properties = properties;
-    }
-
-    /**
-     * Adds a new property to the properties field
-     * @param {string} name 
-     * @param {*} value 
-     * @throws {TypeError} Thrown if the name param is not a string
-     */
-    addProperty( name, value ) {
-        if ( typeof name !== "string" ) {
-            throw new TypeError( `Type of name parameter "${name}" must be string!` );
-        }
-        this.properties[name] = JSON.stringify( value );
-    }
-
-    /**
-     * Sets the geometry field to a new SQLGeometry object
-     * @param {SQLGeometry} geometry 
-     */
-    setGeometry( geometry ) {
-        this.geometry = SQLGeometry.createAnyType( geometry );
+    constructor ( data ) {
+        this.properties = data.properties;
+        this.geometry = SQLGeometry.createAnyType( data.geometry );
     }
 }
