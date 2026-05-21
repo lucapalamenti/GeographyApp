cb61768b578328060e1fa09449334cebd2d94484
lucapalamenti <luca.palamenti@gmail.com>
2026-05-21 15:20:10 -0400

    templateMap for required map information from frontend

---
+++ app/src/api/models/TemplateMap.js
@@ -0,0 +1,31 @@
+module.exports = class TemplateMap {
+    /** @type {string} */
+    map_name = null;
+    /** @type {string} */
+    region_type = null;
+    /** @type {string} */
+    region_name_key = null;
+    /** @type {string | undefined} */
+    #region_parent_name_key = null;
+
+    /**
+     * 
+     * @param {TemplateMap} data 
+     */
+    constructor ( data ) {
+        this.map_name = data.map_name;
+        this.region_type = data.region_type;
+        this.region_name_key = data.region_name_key;
+        this.#region_parent_name_key = data.region_parent_name_key;
+    }
+
+    getRegionParentNameKey() {
+        return this.#region_parent_name_key ? this.#region_parent_name_key : "Earth";
+    }
+
+    verifyState() {
+        if ( !this.map_name || !this.region_type || !this.region_name_key ) {
+            throw new Error( 'All required TemplateMap fields are not populated!' );
+        }
+    }
+}
