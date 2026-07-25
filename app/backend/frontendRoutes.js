5928716cfebba3b099769c2464d00534e6c8e274
lucapalamenti <luca.palamenti@gmail.com>
2026-07-25 15:58:50 -0400

    moved templates and static into frontend folder

--- app/backend/frontendRoutes.js
+++ app/backend/frontendRoutes.js
@@ -2,24 +2,24 @@ const express = require('express');
 const frontendRouter = express.Router();
 
 // Designate the static folder as serving static resources
-frontendRouter.use(express.static('static'));
+frontendRouter.use(express.static('frontend/static'));
 
-const html_dir = "/app/templates/";
+const templates_dir = "/app/frontend/templates/";
 
 frontendRouter.get('/', (req, res) => {
-    res.sendFile(`${html_dir}index.html`);
+    res.sendFile(`${templates_dir}index.html`);
 });
 
 frontendRouter.get('/game', (req, res) => {
-    res.sendFile(`${html_dir}game.html`);
+    res.sendFile(`${templates_dir}game.html`);
 });
 
 frontendRouter.get('/create', (req, res) => {
-    res.sendFile(`${html_dir}create.html`);
+    res.sendFile(`${templates_dir}create.html`);
 });
 
 frontendRouter.get('/admin', (req, res) => {
-    res.sendFile(`${html_dir}admin.html`);
+    res.sendFile(`${templates_dir}admin.html`);
 });
 
 module.exports = frontendRouter;
